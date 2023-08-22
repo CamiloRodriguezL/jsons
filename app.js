6e8b87399bfb9f1c0bdd981dcaed9a232b860e62
CamiloRodriguezL <camilo.rodriguez@thenismile.com>
2023-08-22 13:24:04 -0500

    hi

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 app.use( cors() )
 app.use( express.json() )
 
+// hello
+
 const types = {
     bone: {
         "1": ["low grade and intracompartmental chondrosarcoma"], 
